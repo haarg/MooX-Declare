a7e4bf4bfb01939e8e7c1fa48e64ad19fad6f743
Graham Knop <haarg@haarg.org>
2013-06-14 17:22:09 -0400

    initial commit

---
+++ lib/MooX/Declare.pm
@@ -0,0 +1,42 @@
+package MooX::Declare;
+use strict;
+use warnings;
+# ABSTRACT: A new module
+
+our $VERSION = '0.000001';
+$VERSION = eval $VERSION;
+
+1;
+__END__
+
+=head1 NAME
+
+MooX::Declare - A new module
+
+=head1 SYNOPSIS
+
+  use MooX::Declare;
+
+=head1 DESCRIPTION
+
+A new module.
+
+=head1 AUTHOR
+
+haarg - Graham Knop (cpan:HAARG) <haarg@haarg.org>
+
+=head2 CONTRIBUTORS
+
+None yet.
+
+=head1 COPYRIGHT
+
+Copyright (c) 2013 the L</AUTHOR> and L</CONTRIBUTORS>
+as listed above.
+
+=head1 LICENSE
+
+This library is free software and may be distributed under the same terms
+as perl itself.
+
+=cut
